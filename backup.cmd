bb8bbfefd4a2376c274f3fb56fe7eb52b880adb5
devizer <vgoyda@gmail.com>
2016-07-16 13:35:40 +0300

    Building
    Former-commit-id: d38767dbdb57022f72c5602fa5ebb5da3c3fdaf0

---
+++ backup.cmd
@@ -0,0 +1,9 @@
+taskkill /f /im h3control.exe
+rd /q /s H3Control\bin
+rd /q /s H3Control\obj
+rem winrar a "C:\Users\Пользователь\Google Диск\backup\H3Control (.rar" -s -m5 -agYYYY-MM-DD,HH-MM-SS) -r
+set /p Build=<build\build
+set /p Version=<build\ver
+for /f %%i in ('datetime local') do set datetime=%%i
+"C:\Program Files\7-Zip\7zG.exe" a -t7z -mx=9 -mfb=128 -md=128m -ms=on ^
+  "C:\Users\Backups on Google Drive\H3Control-bin (%datetime%) %Version%.%Build%.7z" .
