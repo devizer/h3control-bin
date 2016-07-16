0590f562d0fa26c9f993dca61caf2617b1bb32a6
Build Server <pi@orange.home>
2016-07-16 14:26:14 +0300

    Building and deploing staging script: (1.22.466)
    Former-commit-id: b69b7813d322e033742793c532c8c3569222ad17

--- build/target/h3control-console.sh
+++ build/target/h3control-console.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # mono --desktop ./bin/H3Control.exe "$@"
+command -v mono >/dev/null || ( echo mono NOT FOUND. Check mono executable is installed and present in the \$PATH ; exit 0 )
+command -v dirname >/dev/null || ( echo dirname NOT FOUND. Check dirname executable is present in the \$PATH ; exit 0 )
 
 pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd`
