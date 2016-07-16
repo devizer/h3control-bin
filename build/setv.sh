a2897b17ffb39109e868c21546b1c75bb4c30555
Build Server <pi@orange.home>
2016-07-16 07:25:19 +0300

    Building and deploing staging script: (1.22.451)

--- build/setv.sh
+++ build/setv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 ver=$(cat ver)
 build=$(cat build)
+build=$(( $build + 1 ))
+echo $build > build
 git pull || true
 git commit -am "Building and deploing staging script: ($ver.$build)" || true
 git push
