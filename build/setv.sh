ad9fbca57d7d577a8080cc0f4aca1091e55911d2
Build Server <pi@orange.home>
2016-07-17 01:21:26 +0300

    Build scripts and murkup updates (1.22.499)
    Former-commit-id: ce8ec0f33a9902e5634608abac60c103feef5f01

--- build/setv.sh
+++ build/setv.sh
@@ -5,6 +5,6 @@ build=$(( $build + 1 ))
 echo $build > build
 echo NEW h3control version is $ver.$build
 git pull || true
-git commit -am "Building and deploing staging script: ($ver.$build)" || true
+git commit -am "Build scripts and murkup updates ($ver.$build)" || true
 git push
 sleep 2
