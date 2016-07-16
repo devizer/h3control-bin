b9116b771e61669c6aa8d8a086c0f713845dec8d
Build Server <pi@orange.home>
2016-07-16 17:28:11 +0300

    Building and deploing staging script: (1.22.475)
    Former-commit-id: 9b75de0e61e7a24509b1a7888999ca7e72748db2

--- build/setv.sh
+++ build/setv.sh
@@ -3,6 +3,7 @@ ver=$(cat ver)
 build=$(cat build)
 build=$(( $build + 1 ))
 echo $build > build
+echo NEW h3control version is $ver.$build
 git pull || true
 git commit -am "Building and deploing staging script: ($ver.$build)" || true
 git push
