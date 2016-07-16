ac7ad7104b1e56aad73457bf2688370fbab8fd84
Build Server <pi@orange.home>
2016-07-16 06:44:43 +0300

    Version assigned manually: 1.22.449

--- build/setv.sh
+++ build/setv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 ver=$(cat ver)
 build=$(cat build)
-git pull
-git commit -am "Version assigned manually: $ver.$build" || git push
+git pull || true
+git commit -am "Version assigned manually: $ver.$build" || true
+git push
