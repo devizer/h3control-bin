9d075506de4d79debc7bab05e75c94c0095bd3c3
Build Server <pi@orange.home>
2016-07-16 14:39:09 +0300

    Building and deploing staging script: (1.22.467)
    Former-commit-id: 85e92e900c3235b31171b0dd9bcb165205af4a91

--- build/target/h3control-install-daemon.sh
+++ build/target/h3control-install-daemon.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+command -v mono >/dev/null || ( echo mono NOT FOUND. Check `mono' executable is installed and present in the \$PATH ; exit 1 )
+command -v dirname >/dev/null || ( echo dirname NOT FOUND. Check `dirname' executable is present in the \$PATH ; exit 1 )
+command -v pwd >/dev/null || ( echo pwd NOT FOUND. Check `pwd' executable is present in the \$PATH ; exit 1 )
+command -v mcs >/dev/null || ( echo mcs NOT FOUND. Check `mcs' executable is present in the \$PATH ; exit 1 )
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
 mkdir -p $HOME/bin
 
 # ***********************************************
 # * The line below specifies deployment folder. *
 # ***********************************************
 cd $HOME/bin
-wget -nv -O h3control.tar.gz --no-check-certificate https://www.dropbox.com/s/o8t38f4yszi06jm/h3control.tar.gz?dl=1
+wget -O h3control.tar.gz --no-check-certificate https://www.dropbox.com/s/o8t38f4yszi06jm/h3control.tar.gz?dl=1
 killall -q -12 mono || echo "h3control isn't running"
 rm -rf h3control
 tar xzf h3control.tar.gz
