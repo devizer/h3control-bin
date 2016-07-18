13c52a1ec04a354cd7ec2d715e7ec5711b294ece
devizer <noreply@devizer.sb>
2016-07-18 06:34:09 +0300

    Staging update: v1.22.537, staging updates aren't recommended for upgrade
    Former-commit-id: 619900c69eb5f5aa242c05daa131af0399df3fc9

--- build/build-staging.sh
+++ build/build-staging.sh
@@ -24,7 +24,8 @@ echo "[assembly: System.Reflection.AssemblyVersion(\"$ver.$build.0\")]" > $src/h
 # root of repo
 cd h3control
 packages=/m/v/_GIT/h3control/packages
-cp -R $packages .
+# cp -R $packages .
+nuget restore H3Control.sln 
 rm -rf H3Control/{bin,obj}
 time ( xbuild H3Control.sln /t:Rebuild /p:Configuration=Release /verbosity:normal )
 nu-c -labels ./H3Control.Tests/bin/Release/H3Control.Tests.dll | tee H3Control.Tests.dll.log
