140f63a41397cd3e71dd4831d24ebd0812ab6e73
devizer <noreply@devizer.sb>
2016-07-25 18:34:13 +0300

    Build scripts and murkup updates (1.24.657)
    Former-commit-id: a5c712939d881ba814cebec2bb15575ce8d6398a

--- build/build-staging.sh
+++ build/build-staging.sh
@@ -89,7 +89,7 @@ echo ''
 cd $src/..
 rm -rf *
 
-{ wget -q -nv -O - https://github.com/devizer/h3control-bin/raw/master/staging/h3control-staging.sh | bash } &
+( wget -q -nv -O - https://github.com/devizer/h3control-bin/raw/master/staging/h3control-staging.sh | bash ) &
 disown
 echo DISOWNED
 sleep 22
