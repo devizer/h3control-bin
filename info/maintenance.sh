47c29e6cc8a53bebc295d08206aeffd08d7f4866
devizer <noreply@devizer.sb>
2016-07-25 22:44:32 +0300

    commit before maintenance
    Former-commit-id: 3a8e5825ff15f3d5b34357bdc160da760cbf840c
    Former-commit-id: 8db10a1479c7c3e11e3a0fb512e6fcf8c13be500

--- info/maintenance.sh
+++ info/maintenance.sh
@@ -1,7 +1,7 @@
 git pull
 git commit -am "commit before maintenance"
 wget -O /tmp/bfg.jar http://repo1.maven.org/maven2/com/madgag/bfg/1.12.12/bfg-1.12.12.jar
-java -jar /tmp/bfg.jar --strip-blobs-bigger-than 2900K $(dirname `pwd`)
+java -jar /tmp/bfg.jar --strip-blobs-bigger-than 3M $(dirname `pwd`)
 
 rm /tmp/bfg.jar
 git push
