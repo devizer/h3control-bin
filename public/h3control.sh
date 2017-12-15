492413a25435f01055a222e0a7ab091812a77486
Volodymyr Hoida <devizer@users.github.com>
2017-12-15 07:52:17 +0300

    Fixed upgrade
    Former-commit-id: 172439c4cdf27debc6e133690e7acd90200ab7ba

--- public/h3control.sh
+++ public/h3control.sh
@@ -9,7 +9,7 @@ sudo systemctl stop h3control >/dev/null 2>&1 || true
 pid=$(cat $pidfile 2>/dev/null)
 if [ -n "$pid" ]; then
   rm -f $pidfile >/dev/null 2>&1 || true
-  sudo kill -12 $pid >/dev/null 2>&1 || true
+  sudo kill $pid >/dev/null 2>&1 || true
 else
   sudo killall -q -s 12 mono >/dev/null 2>&1 || true
 fi
