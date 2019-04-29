f386dc6bea6fc9678fad2c9abfa5a0435ec28f27
devizer <devizer@github.io>
2019-04-29 09:37:44 +0300

    added public/status.svg
    Former-commit-id: 170993030aee84898fb2ac66d14d2956af1cad2d

---
+++ public/h3control-staging.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd /tmp
+wget -O h3control.tar.gz https://github.com/devizer/h3control-bin/raw/master/staging/h3control.tar.gz
+
+pidfile=/var/run/h3control.pid
+
+# Stop Prev Version
+sudo systemctl stop h3control >/dev/null 2>&1 || true
+pid=$(cat $pidfile 2>/dev/null)
+if [ -n "$pid" ]; then
+  rm -f $pidfile >/dev/null 2>&1 || true
+  sudo kill $pid >/dev/null 2>&1 || true
+else
+  sudo killall -q mono >/dev/null 2>&1 || true
+fi
+
+rm -rf h3control
+tar xzf h3control.tar.gz
+bash h3control/h3control-console.sh --pid-file=$pidfile --binding=*:5000
